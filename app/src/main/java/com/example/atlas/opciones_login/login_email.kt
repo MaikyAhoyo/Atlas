a13d257bb92b11ee467311e747a80e77fe70508a
Maiky-ahoyo <arroyomiguel590@gmail.com>
2026-02-03 12:31:15 -0700

    dark mode png and register

--- app/src/main/java/com/example/atlas/opciones_login/login_email.kt
+++ app/src/main/java/com/example/atlas/opciones_login/login_email.kt
@@ -1,21 +1,22 @@
 package com.example.atlas.opciones_login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.atlas.R
+import com.example.atlas.RegistroEmail
+import com.example.atlas.databinding.ActivityLoginEmailBinding
 
 class login_email : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginEmailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login_email)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.TxtRegistrarme.setOnClickListener {
+            startActivity(Intent(this@login_email, RegistroEmail::class.java))
         }
     }
 }
