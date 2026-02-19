a983187773fd7701329258cd7cdefd20f362bffe
Maiky-ahoyo <arroyomiguel590@gmail.com>
2026-02-19 12:40:23 -0700

    google auth

--- app/src/main/java/com/example/atlas/OpcionesLogin.kt
+++ app/src/main/java/com/example/atlas/OpcionesLogin.kt
@@ -1,10 +1,12 @@
 package com.example.atlas
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.atlas.databinding.ActivityOpcionesLoginBinding
 import com.example.atlas.opciones_login.login_email
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.firebase.auth.FirebaseAuth
 
 class OpcionesLogin : AppCompatActivity() {
@@ -13,10 +15,18 @@ class OpcionesLogin : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
 
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOpcionesLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
 
         firebaseAuth = FirebaseAuth.getInstance()
         comprobarSesion()
