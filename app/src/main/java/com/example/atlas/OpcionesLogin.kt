ddcc0e04f1fdf4d16fdb2bd87a92080aee64d6b5
Maiky-ahoyo <arroyomiguel590@gmail.com>
2026-02-26 13:33:44 -0700

    edit profile

--- app/src/main/java/com/example/atlas/OpcionesLogin.kt
+++ app/src/main/java/com/example/atlas/OpcionesLogin.kt
@@ -3,20 +3,23 @@ package com.example.atlas
 import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.atlas.databinding.ActivityOpcionesLoginBinding
-import com.example.atlas.opciones_login.login_email
+import com.example.atlas.opciones_login.LoginEmail
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class OpcionesLogin : AppCompatActivity() {
 
     private lateinit var binding: ActivityOpcionesLoginBinding
-
     private lateinit var firebaseAuth: FirebaseAuth
-
     private lateinit var mGoogleSignInClient: GoogleSignInClient
-
     private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +34,63 @@ class OpcionesLogin : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         comprobarSesion()
 
-        binding.IngresarEmail.setOnClickListener {
-            startActivity(Intent(this@OpcionesLogin, login_email::class.java))
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.IngresarGoogle.setOnClickListener {
+            googleLogin()
         }
+
+        binding.IngresarEmail.setOnClickListener {
+            startActivity(Intent(this@OpcionesLogin, LoginEmail::class.java))
+        }
+    }
+
+    private fun googleLogin(){
+        progressDialog.setMessage("Conectando con Google...")
+        progressDialog.show()
+        val googleSignInIntent = mGoogleSignInClient.signInIntent
+        googleSingInARL.launch(googleSignInIntent)
+    }
+
+    private val googleSingInARL = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){ resultado ->
+        if(resultado.resultCode == RESULT_OK){
+            val data = resultado.data
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val cuenta = task.getResult(ApiException::class.java)
+                autenticacionGoogle(cuenta.idToken)
+            }catch (e: Exception){
+                progressDialog.dismiss()
+                Toast.makeText(this, "${e.message }", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            progressDialog.dismiss()
+        }
+    }
+
+    private fun autenticacionGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnSuccessListener { resultadoAuth ->
+                progressDialog.dismiss()
+                if (resultadoAuth.additionalUserInfo!!.isNewUser) {
+                    startActivity(Intent(this, CompletarPerfil::class.java))
+                    finish()
+                } else {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finishAffinity()
+                }
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun comprobarSesion(){
