5e7cb9dac390f2af2de5ea79acff37de8ab7a095
verorodzn <a222215753@unison.mx>
2026-05-13 12:17:09 -0700

    Chat

--- app/src/main/java/com/example/atlas/Fragmentos/FragmentChat.kt
+++ app/src/main/java/com/example/atlas/Fragmentos/FragmentChat.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentProgreso.newInstance] factory method to
+ * Use the [FragmentRutinas.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentProgreso : Fragment() {
+class FragmentChat : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class FragmentProgreso : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progreso, container, false)
+        return inflater.inflate(R.layout.fragment_rutinas, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class FragmentProgreso : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentProgreso.
+         * @return A new instance of fragment FragmentRutinas.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentProgreso().apply {
+            FragmentRutinas().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
